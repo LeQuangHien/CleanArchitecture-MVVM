2314b6b45d4f73679e35a0b67b6f7d72b5511703
Hien Le <lequanghien247@gmail.com>
2025-04-07 23:12:23 +0200

    Use type-safety route

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlin.serialization.json)
 
     implementation(libs.bundles.koin)
 
--- app/src/main/kotlin/com/hienle/cleanarchitecture/CaScreen.kt
+++ app/src/main/kotlin/com/hienle/cleanarchitecture/CaScreen.kt
@@ -1,34 +1,43 @@
 package com.hienle.cleanarchitecture
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.*
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.hienle.cleanarchitecture.feature.news.article.NewsScreen
 import com.hienle.cleanarchitecture.feature.news.webview.WebViewScreen
-import java.net.URLEncoder
+import kotlinx.serialization.Serializable
 
-// Define sealed classes for navigation destinations
-sealed class CaScreen(@StringRes val title: Int, val route: String) {
-    object Start : CaScreen(R.string.app_name, "start")
-    object Details : CaScreen(R.string.details_screen, "details/{url}") {
-        fun createRoute(url: String) = "details/$url"
-    }
-    object Favorites : CaScreen(R.string.favorites_screen, "favorites")
-}
+@Serializable
+object Start
+
+@Serializable
+data class Details(val url: String)
+
+@Serializable
+object Favorites
 
 /**
  * Composable that displays the topBar and displays back button if back navigation is possible.
@@ -36,17 +45,17 @@ sealed class CaScreen(@StringRes val title: Int, val route: String) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CaAppBar(
-    currentScreen: CaScreen,
+    currentScreen: String,
     canNavigateBack: Boolean,
     navigateUp: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     TopAppBar(
-        title = { Text(stringResource(currentScreen.title)) },
+        title = { Text(currentScreen) },
         colors =
-        TopAppBarDefaults.mediumTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-        ),
+            TopAppBarDefaults.mediumTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+            ),
         modifier = modifier,
         navigationIcon = {
             if (canNavigateBack) {
@@ -66,13 +75,14 @@ fun CaApp(navController: NavHostController = rememberNavController()) {
     // Get current back stack entry
     val backStackEntry by navController.currentBackStackEntryAsState()
     // Get the current screen from the back stack entry's route
-    val currentScreen =
-        when (backStackEntry?.destination?.route?.substringBefore("/")) {
-            CaScreen.Start.route -> CaScreen.Start
-            CaScreen.Details.route.substringBefore("/{url}") -> CaScreen.Details
-            CaScreen.Favorites.route -> CaScreen.Favorites
-            else -> CaScreen.Start
+    val currentScreen = backStackEntry?.destination?.let { currentDestination ->
+        when {
+            currentDestination.hasRoute(Start::class) -> "Start"
+            currentDestination.hasRoute(Details::class) -> "Details"
+            currentDestination.hasRoute(Favorites::class) -> "Favorites"
+            else -> "Start"
         }
+    } ?: "Start"
 
     Scaffold(
         topBar = {
@@ -86,35 +96,31 @@ fun CaApp(navController: NavHostController = rememberNavController()) {
 
         NavHost(
             navController = navController,
-            startDestination = CaScreen.Start.route,
+            startDestination = Start,
             modifier =
-            Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
+                Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
         ) {
-            composable(route = CaScreen.Start.route) {
+            composable<Start> {
                 NewsScreen(
                     onArticleClicked = { url ->
-                        val encodedUrl = URLEncoder.encode(url, Charsets.UTF_8.name())
-                        navController.navigate(CaScreen.Details.createRoute(encodedUrl))
+                        navController.navigate(Details(url))
                     },
                     modifier =
-                    Modifier
-                        .fillMaxSize()
-                        .padding(dimensionResource(R.dimen.padding_medium)),
+                        Modifier
+                            .fillMaxSize()
+                            .padding(dimensionResource(R.dimen.padding_medium)),
                 )
             }
-            composable(
-                route = CaScreen.Details.route,
-                arguments = listOf(navArgument("url") { type = NavType.StringType }),
-            ) { navBackStackEntry ->
-                val url = navBackStackEntry.arguments?.getString("url") ?: ""
+            composable<Details> { navBackStackEntry ->
+                val details: Details = navBackStackEntry.toRoute()
                 WebViewScreen(
-                    url = url,
+                    url = details.url,
                     modifier = Modifier.fillMaxHeight(),
                 )
             }
-            composable(route = CaScreen.Favorites.route) {
+            composable<Favorites> {
                 FavoritesScreen()
             }
         }
